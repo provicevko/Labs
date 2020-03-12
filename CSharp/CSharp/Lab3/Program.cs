cea673d07d92797a143781e70376f7ebedafff3f
Provice <provicevk@gmail.com>
2020-03-12 12:11:16 +0200

    +lab3

---
+++ CSharp/CSharp/Lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            TwoDimensionalArray matrix = new TwoDimensionalArray();
+            Console.WriteLine(matrix[7,1]);
+            Console.WriteLine(matrix.CountCols);
+            Console.WriteLine(matrix.CountRows);
+        }
+    }
+}
---
+++ CSharp/CSharp/Lab3/TwoDimensionalArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab3
+{
+    public class TwoDimensionalArray
+    {
+        private int Rows;
+        private int Cols;
+        private int[,] Matrix = //diapazone [-2147483647;2147483647].   -2147483648 => false value;
+        {
+            {1, 546, 68, 3, 5, 78},
+            {5, 4, 21, 24, 12, 79},
+            {1, 56, 11, 3, 3, 95},
+            {76, 36, 14, 37, 92, 23},
+            {8, 5, 76, 2, 34, 11},
+            {34, 23, 2, 124, 56, 856}
+        };
+
+        public int CountRows => Rows;
+        public int CountCols => Cols;
+
+        public TwoDimensionalArray()
+        {
+            this.Rows = this.Matrix.GetLength(0);
+            this.Cols = this.Matrix.GetLength(1);
+        }
+        public int this[int i, int j]
+        {
+            private set
+            {
+                int shift = 5;
+                Matrix[i + shift, j + shift] = value;
+            }
+            get
+            {
+                int shift = 5;
+                
+                if (i >= shift && j >= shift && i <= this.Rows + shift && j <=
+                    this.Cols+shift)
+                {
+                    return Matrix[i-shift, j-shift];
+                }
+                else
+                {
+                    return Int32.MinValue;
+                }
+            }
+        }
+    }
+}
